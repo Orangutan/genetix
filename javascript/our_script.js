8b7da4eb4010b8ae0650ad98661dc568523359dd
Orangutan <espen@primate.co.uk>
2016-02-11 20:50:34 +0000

    added objects and multiple gene combinations

--- javascript/our_script.js
+++ javascript/our_script.js
@@ -1,61 +1,104 @@
 window.onload = function() {
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
-    
-    // for(var i = 0; i < 5; i+=1) {
-    //     var multiplier = i*5;
-    //     paper.circle(250 + (2*multiplier), 100 + multiplier, 50 - multiplier);
-    // }
-    
-    
-    // var gene = [[0,1,1,0],
-    //             [1,1,1,1],
-    //             [0,0,0,0],
-    //             [1,0,0,1]];
+  var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
 
-    for(var i = 0; i <= 15; i +=1) {
-      var hor = 0;
-      var ver = 0;
-      var gene = [0,1,1,0,1,1,1,1,0,0,0,0,1,0,0,1];
-      
-      if(i > 11) {
-        ver = 40;
-        hor = (i-12)*10;
-      } else if(i > 7) {
-        ver = 30;
-        hor = (i-8)*10;
-      } else if(i > 3) {
-        ver = 20;
-        hor = (i-4)*10;
-      } else {
-        hor = i*10;
-        ver = 10;
-      }
-      
-      var matrix = paper.rect(hor, ver, 10, 10);
-      matrix.attr({'stroke-width': 0})
-      
-      if(gene[i]){
-        matrix.attr({fill: 'black'});
+  // var monkey = [0,1,1,0,1,1,1,1,0,0,0,0,1,0,0,1];
+  
+  var monkey = {
+    genome: [0,1,1,0,1,1,1,1,0,0,0,0,1,0,0,1],
+    physique: [2,2,3]
+  }
+  
+  var whale = {
+    genome: [1,1,1,1,1,0,0,1,1,0,0,1,1,0,0,1],
+    physique: [3,1,2]
+  }
+  
+  var piranha = {
+    genome: [0,1,1,0,1,1,1,1,1,1,1,1,1,0,0,1],
+    physique: [1,3,1]
+  }
+  
+  var cuckoo = {
+    genome: [0,1,1,0,1,1,1,1,0,1,1,0,0,1,1,0],
+    physique: [1,2,2]
+  }
+  
+  var newCreature = function(attack, defence, body) {
+    
+    var size = body.physique[0];
+    var speed = body.physique[2];
+    var stamina = body.physique[3];
+    
+    var top = function(genome) {
+
+      for(var i = 0; i <= 15; i +=1) {
+
+        var hor = 0;
+        var ver = 0;
+        var sq = 6+(size*4);
+        
+        if(i > 11) {
+          ver = sq*4;
+          hor = (i-12)*sq;
+        } else if(i > 7) {
+          ver = sq*3;
+          hor = (i-8)*sq;
+        } else if(i > 3) {
+          ver = sq*2;
+          hor = (i-4)*sq;
+        } else {
+          hor = i*sq;
+          ver = sq;
+        }
+
+        var matrix = paper.rect(hor, ver, sq, sq);
+        matrix.attr({'stroke-width': 0})
+
+        if(genome[i]){
+          matrix.attr({fill: 'black'});
+        }
       }
     }
+    top(attack.genome);
     
-    // for(var i = 1; i <= 4; i +=1) {
-    //   var hor = i*10;
-    //
-    //   paper.rect(hor, 20, 10, 10);
-    // }
-    //
-    // for(var i = 1; i <= 4; i +=1) {
-    //   var hor = i*10;
-    //
-    //   paper.rect(hor, 30, 10, 10);
-    // }
-    //
-    // for(var i = 1; i <= 4; i +=1) {
-    //   var hor = i*10;
-    //
-    //   paper.rect(hor, 40, 10, 10);
-    // }
+    var bottom = function(genome) {
     
+      genome.reverse();
     
+      for(var i = 0; i <= 15; i +=1) {
+        var hor = i*10;
+        var ver = 0;
+        
+        var hor = 0;
+        var ver = 0;
+        var sq = 6+(size*4);
+    
+        if(i > 11) {
+          ver = sq*4;
+          hor = (i-12)*sq;
+        } else if(i > 7) {
+          ver = sq*3;
+          hor = (i-8)*sq;
+        } else if(i > 3) {
+          ver = sq*2;
+          hor = (i-4)*sq;
+        } else {
+          hor = i*sq;
+          ver = sq;
+        }
+    
+        var matrix = paper.rect(hor, (ver + 4*sq), sq, sq);
+        matrix.attr({'stroke-width': 0})
+      
+        if(genome[i]){
+          matrix.attr({fill: 'black'});
+        }
+      }
+    }
+    bottom(defence.genome);
+    
+  };
+  
+  newCreature(cuckoo, monkey, whale);
+
 }
